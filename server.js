4744db16335679d4b4038d30a21de6a50aea1116
Furkan YILMAZOGLU <88587274+FurkanY94@users.noreply.github.com>
2025-06-25 16:21:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 // Public klasörünü servis et
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Ana sayfayı gönder
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Oyuncu verilerini tut
 let players = {};
